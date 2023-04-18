034201db936a1cc05ccf39f4044ccbb8e8bb20f1
vinson昊 <jsvinson@163.com>
2023-04-18 11:12:30 +0000

    修改全局文件夹别名引用

--- src/main/webapp/vue.config.js
+++ src/main/webapp/vue.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+function resolve (dir) {
+  //此处使用path.resolve 或path.join 可自行调整
+  return path.join(__dirname, dir)
+}
 module.exports = {
+    chainWebpack: config => {
+        config.resolve.alias.set('@', resolve('src'))
+    },
     devServer: {
         proxy: "http://127.0.0.1:8889", //开发环境的跨域问题解决
         port: 8181
-    }
+    },
 }
